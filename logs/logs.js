9f993831a60e2bb57840427a386b28436f7c244d
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-03-12 01:16:45 +0530

    completed Logs pages with Network and error monitoring

--- logs/logs.js
+++ logs/logs.js
@@ -4,6 +4,124 @@ const page1SubmitKey = document.querySelector(".btn-submit")
 const page1copy1 = document.querySelectorAll("#page1copy1")
 const page1show1 = document.querySelector("#page1show1")
 const btnRight = document.querySelector(".btnRight")
+const h3 = document.getElementById("h3")
+
+//a Super useful function that converts bytes to Size
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    if (i == 0) return bytes + ' ' + sizes[i];
+    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+};
+//console.log(bytesToSize(108)) =>108 Bytes ;  //console.log(bytesToSize(1123233222)); => 1.0 GB
+
+//hiding the textfield and copy button by default
+page1copy1[0].style.display="none"
+page1copy1[1].style.display="none"
+//hiding view button by default
+btnRight.style.display ="none"
+
+
+//api configuration
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDFCN2Q5RTk4Qzg1Njc4MWVhRDJEMzBBNjkzYkU0NjgzM2E0Mjg1QjciLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Nzc3NDMxOTI3OTIsIm5hbWUiOiJESVMifQ.B-zCBx7XBk-5KVDyL-Vgdk1T2zOuBQ3b7QMfPTDRc-M");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+page1data1.addEventListener('click',(e) => {
+    e.preventDefault()
+
+    //using the api call to get info of all uploads by user
+    fetch("https://api.web3.storage/user/uploads", requestOptions)
+    .then(response => {
+        console.log(`Response status: ${response.status}`); // log the response status code
+        return response.text();
+      })
+  .then(result => {
+    
+    //parsing json as a string
+    let JsonArray = JSON.parse(result)
+
+    //var output for storing all data from json array for the textarea.
+    var output = '';
+
+    //using for loop to print data from each response
+    for (let i = 0; i < JsonArray.length; i++) {
+        const obj = JsonArray[i];
+
+        const dCID =  obj.cid;
+        const dOnIPFS = (obj.type === "Car") ? "In Transit" : (obj.type === "Upload") ? "Uploaded" : null; //This uses the ternary operator to check the value of obj.type. If it's "Car", then dStatus is set to "In Transit". If it's "Upload", then dStatus is set to "Uploaded". If it's neither "Car" nor "Upload", then dStatus is set to null.
+        const dSize = bytesToSize(obj.dagSize);
+        const dCreated = (moment(obj.created).format('llll'))    //see at bottom
+        const dPeer = obj.pins[0].peerName;
+        const dPeerId = obj.pins[0].peerId;
+        
+        console.log(`CID: ${dCID}, On IPFS: ${dOnIPFS}, Size: ${dSize}, Created: ${dCreated}, Peer: ${dPeer}, PeerId:${dPeerId}`);
+      
+        //showing Json response on webpage
+        const NewDiv = document.createElement('div');
+        NewDiv.innerHTML = `<centre><span class="title">Upload Log [${i+1}] -</span></centre><br><p><span class="bold">CID:</span> ${dCID}<br><span class="bold">On IPFS:</span> ${dOnIPFS}<br> <span class="bold">Size:</span> ${dSize}<br><span class="bold">Created:</span> ${dCreated}<br><span class="bold">Peer:</span> ${dPeer}<br><span class="bold">PeerId:</span> ${dPeerId}<p>`;
+        //NewDiv.innerHTML = JSON.stringify(result)
+        NewDiv.classList.add('list-item');
+        page1show1.appendChild(NewDiv);
+    
+        //append that in the textarea
+        output += `CID: ${dCID} \n OnIPFS: ${dOnIPFS} \n Size: ${dSize} \n Created: ${dCreated} \n Peer: ${dPeer} \n PeerId: ${dPeerId} \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n`;
+    }
+    
+    //set the value of textarea with all outputs from json array
+    page1copy1[0].value = output
+    page1copy1[0].style.display="block"
+    page1copy1[1].style.display="block"
+
+    console.table(output);
+
+    //making the view button visible
+    btnRight.style.display="block"
+    //hiding the submit button
+    page1data1.style.display="none"
+    h3.innerText = "Information of all Uploads- "
+    //console.log(result)
+
+    
+})
+  .catch(error => {
+    console.error('error', error)
+    h3.innerHTML = '<centre><h1 style="color: red;">Error Occured \n Please Check your Internet</h1></centre>'
+    page1data1.style.display="none"
+});
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // //a Super useful function that converts bytes to Size
 // function bytesToSize(bytes) {
